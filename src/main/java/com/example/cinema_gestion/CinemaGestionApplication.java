dcb58ef7510c45462bd33e9a2c875f1946bfa4a9
DESKTOP-DB7C544\asus <ranimloutar1@gmail.com>
2023-05-10 17:06:02 +0100

    backk

---
+++ src/main/java/com/example/cinema_gestion/CinemaGestionApplication.java
@@ -0,0 +1,13 @@
+package com.example.cinema_gestion;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CinemaGestionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CinemaGestionApplication.class, args);
+	}
+
+}
